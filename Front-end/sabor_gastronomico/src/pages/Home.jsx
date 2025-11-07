b101f04d23e030b745c2d4f865f1c3615da09d11
fiovargas <fiovargasp31@gmail.com>
2025-11-07 13:33:59 -0600

    Inicio 1

---
+++ Front-end/sabor_gastronomico/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Inicio from '../components/Inicio/Inicio'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+
+function Home() {
+  return (
+    <div>
+      <Inicio/>
+      <Header/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
