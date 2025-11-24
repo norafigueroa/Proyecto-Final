2dcebdf40051e7510d3f0b00f4717c53bcdccee2
fiovargas <fiovargasp31@gmail.com>
2025-11-24 11:10:01 -0600

    Act Fio

--- Front-end/sabor_gastronomico/src/components/InicioAdminGeneral/InicioAdminGeneral.jsx
+++ Front-end/sabor_gastronomico/src/components/InicioAdminGeneral/InicioAdminGeneral.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext'; 
+import "./InicioAdminGeneral.css";
 
 function InicioAdminGeneral() {
   const { usuario, logout  } = useAuth(); 
